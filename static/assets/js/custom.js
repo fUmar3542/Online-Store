9d7e89a69e416abd6a072f7d2ec4491f15eeafdb
fumar3542 <fumar3542@outlook.com>
2022-08-23 20:18:30 +0500

    Admin Panel In Progress

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -2,3 +2,14 @@
     Custom JS (Any custom js code you want to apply should be defined here).
 ====================================================================================*/
 
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $(".btn.btn-fill-type").click(function(){
+      //alert("The paragraph was clicked.");
+      $(".modal-window-container.fancy.sub-cat.window-show").css("display","block");
+      $(".modal-window-container").addClass("window-show");
+      $("body").addClass("modal-window-open");
+    });
+});
+
