f630f8f8ddb3a46df1b7d34302579f4af3a5334b
ans naeem <ans.naeem31@gmail.com>
2022-08-23 14:20:30 +0500

    content and blog writing done by Ans naeem

--- Base.py
+++ Base.py
@@ -2,6 +2,7 @@ import pymysql
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('mysql+pymysql://root:1234@localhost:3307/project_data')
+# engine = create_engine('mysql+pymysql://root:1234@localhost:3307/project_data')
+engine = create_engine('mysql+pymysql://root:summer vine@localhost/project-data')
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
